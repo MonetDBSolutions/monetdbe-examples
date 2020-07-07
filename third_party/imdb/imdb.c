791fc458f7d5cf3c97a69aa5508da91b8c561116
svetlin <svetlin.stalinov@monetdbsolutions.com>
2020-07-07 17:13:10 +0200

    imdb example end

--- third_party/imdb/imdb.c
+++ third_party/imdb/imdb.c
@@ -1,35 +1,52 @@
+#include <limits.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "imdb.h"
 #include "imdb_constants.h"
 
-char* imdbgen(monetdbe_database mdbe, char* data_dir){
+static char* copy_into_stmt(char* tbl_name, char* data_file){
+    char buf[strlen(tbl_name)+ strlen(data_file) + 100];
+    sprintf(buf, "COPY INTO \"%s\" FROM '%s' USING DELIMITERS ',' NULL as '' BEST EFFORT;", tbl_name, data_file);
+    return strdup(buf);
 }
 
+static char* data_file_path(char* data_path, char* tbl_name){
+    char buf[PATH_MAX + strlen(tbl_name)];
+    sprintf(buf, "%s/%s.csv.gz", data_path, tbl_name);
+    return strdup(buf);
+}
 
-//using namespace duckdb;
-//using namespace std;
-//
-//namespace imdb {
-//
-//void dbgen(DuckDB &db) {
-//	Connection con(db);
-//	con.Query("BEGIN TRANSACTION");
-//	for (int t = 0; t < IMDB_TABLE_COUNT; t++) {
-//		con.Query(IMDB_TABLE_DDL[t]);
-//		string table_name = string(IMDB_TABLE_NAMES[t]);
-//		string data_file_name = "third_party/imdb/data/"+table_name+".csv.gz";
-//		auto file_system = make_unique<FileSystem>();
-//		if (!file_system->FileExists(data_file_name)) {
-//			throw Exception("IMDB data file missing, try `make imdb` to download.");
-//		}
-//		con.Query("COPY "+table_name+" FROM '"+data_file_name+"' DELIMITER ',' ESCAPE '\\';");
-//	}
-//	con.Query("COMMIT");
-//}
-//
-//string get_query(int query) {
-//	if (query <= 0 || query > IMDB_QUERIES_COUNT) {
-//		throw SyntaxException("Out of range IMDB query number %d", query);
-//	}
-//	return IMDB_QUERIES[query - 1];
-//}
-//} // namespace imdb
+char* imdbgen(monetdbe_database mdbe, char* data_dir){
+    char data_path[PATH_MAX + 1];
+    char *data_file, *q;
+    const char *tbl_name, *tbl_ddl;
+    char* err = NULL;
+    realpath(data_dir, data_path);
+    
+    for(size_t t=0; t < IMDB_TABLE_COUNT; t++){
+        // schema
+        tbl_ddl = IMDB_TABLE_DDL[t]; 
+        tbl_name = IMDB_TABLE_NAMES[t];
+        if((err=monetdbe_query(mdbe,(char*) tbl_ddl, NULL, NULL)) != NULL)
+           return err; 
+        data_file = data_file_path(data_path,(char*) tbl_name); 
+        if(access(data_file, F_OK) != 0) {
+            sprintf(err, "Error: file %s does not exists!", data_file);
+            return err; 
+        }
+        q = copy_into_stmt((char*) tbl_name, data_file);
+        if((err=monetdbe_query(mdbe,(char*) q, NULL, NULL)) != NULL)
+           return err; 
+
+    }
+
+    return NULL;
+}
+
+const char* get_query(int query) {
+	if (query <= 0 || query > IMDB_QUERIES_COUNT) {
+        return NULL;
+	}
+	return IMDB_QUERIES[query - 1];
+}
+
