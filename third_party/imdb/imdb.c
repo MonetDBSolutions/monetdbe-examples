53423306435fe88a2ebf6e54b5486604eb3d7dd2
svetlin <svetlin.stalinov@monetdbsolutions.com>
2020-07-07 11:03:00 +0200

    begin imdb C

---
+++ third_party/imdb/imdb.c
@@ -0,0 +1,35 @@
+#include "imdb.h"
+#include "imdb_constants.h"
+
+char* imdbgen(monetdbe_database mdbe, char* data_dir){
+}
+
+
+//using namespace duckdb;
+//using namespace std;
+//
+//namespace imdb {
+//
+//void dbgen(DuckDB &db) {
+//	Connection con(db);
+//	con.Query("BEGIN TRANSACTION");
+//	for (int t = 0; t < IMDB_TABLE_COUNT; t++) {
+//		con.Query(IMDB_TABLE_DDL[t]);
+//		string table_name = string(IMDB_TABLE_NAMES[t]);
+//		string data_file_name = "third_party/imdb/data/"+table_name+".csv.gz";
+//		auto file_system = make_unique<FileSystem>();
+//		if (!file_system->FileExists(data_file_name)) {
+//			throw Exception("IMDB data file missing, try `make imdb` to download.");
+//		}
+//		con.Query("COPY "+table_name+" FROM '"+data_file_name+"' DELIMITER ',' ESCAPE '\\';");
+//	}
+//	con.Query("COMMIT");
+//}
+//
+//string get_query(int query) {
+//	if (query <= 0 || query > IMDB_QUERIES_COUNT) {
+//		throw SyntaxException("Out of range IMDB query number %d", query);
+//	}
+//	return IMDB_QUERIES[query - 1];
+//}
+//} // namespace imdb
---
+++ third_party/imdb/include/imdb.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "monetdbe.h"
+
+char* imdbgen(monetdbe_database mdbe, char* data_dir);
+
+//! Gets the specified IMDB JOB Query number as a string
+const char* get_query(int query);
+
